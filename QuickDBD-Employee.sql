5d7ff066a02576b8c328511d0c0595f4db7d534e
judsica <64810113+judsica@users.noreply.github.com>
2023-10-15 12:24:30 -0400

    Add files via upload

---
+++ QuickDBD-Employee.sql
@@ -0,0 +1,57 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/D1M4Yp
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Physical
+
+CREATE TABLE "dep_emp" (
+    "emp_no" INT,   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_dep_emp" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" INT   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no","dept_name"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id","title"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" money   NOT NULL
+);
+
+ALTER TABLE "dep_emp" ADD CONSTRAINT "fk_dep_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dep_emp" ADD CONSTRAINT "fk_dep_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "salaries" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "employees" ("emp_title_id");
+
